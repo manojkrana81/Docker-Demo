82544c5d6cda2fd32b10a21a5f9f706f4f8da02b
unknown <manojkrana81@gmail.com>
2024-01-04 21:59:04 -0800

    added endpoint to test docker volume

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const fs = require('fs/promises');
+
+const dataFolder = path.join(__dirname, 'data/files');
+
+fs.mkdir(dataFolder, {recursive: true});
 
 app.get('/ping', (req, res)=>{
     console.log('hey there');
     res.send('pong');
 });
+
+app.get('/create-file', async (req, res) => {
+    try {
+      const fileName = `${(Math.random()*100 + 10)}_file.txt`;
+      const filePath = path.join(dataFolder, fileName);
+  
+      // Write content to a file
+      await fs.writeFile(filePath, 'Hello, Docker Volumes!');
+  
+      res.send('File created successfully!');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 app.listen(3000, ()=>{
     console.log("server started");
